2441cc83c107f01ce097255c8919fc972f7e1535
Nicholas Weber <nicholaseweber@gmail.com>
2018-03-04 19:01:39 -0800

    Add JS to make responsive menu work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// JS here
+function menuToggle() {
+    var topnav = document.getElementById("my-top-nav");
+    if (topnav.className === "topnav") {
+        topnav.className += " responsive";
+    } else {
+        topnav.className = "topnav";
+    }
+}
